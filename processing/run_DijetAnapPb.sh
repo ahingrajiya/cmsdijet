24d6ec8325153404b8c95c74ef2c3a13d254979c
Abhishek Hingrajiya <abhi.h123@gmail.com>
2024-10-28 19:21:09 +0100

    bug fixes

--- processing/run_DijetAnapPb.sh
+++ processing/run_DijetAnapPb.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #Change the path to the CMSSW_13_0_5 directory
-ENV_PATH = ${HOME}/private/analysis/CMSSW_13_0_5/src
-cd ${ENV_PATH}
+CMS_PATH=${HOME}/private/analysis/CMSSW_13_0_5/src
+cd ${CMS_PATH}
 
 #Export the CMSSW environment
 eval 'scramv1 runtime -sh'
 
-EXEC_PATH = ${HOME}/private/analysis/CMSSW_13_0_5/src/cmsdijet/execution_dir
+EXEC_PATH=${HOME}/private/analysis/CMSSW_13_0_5/src/cmsdijet/executions_dir
 cd ${EXEC_PATH}
 
-input_file_list = $1
-output_file = $2
-ismc = $3
-isPbgoing = $4
-pthatmin = $5
-pthatmax = $6
+input_file_list=$1
+output_file=$2
+ismc=$3
+isPbgoing=$4
+pthatmin=$5
+pthatmax=$6
 
 
 ./pPbDijetAna ${input_file_list} ${output_file} ${ismc} ${isPbgoing} ${pthatmin} ${pthatmax}
