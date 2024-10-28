cc12062398e9457a74c4d22e9252dab8f52c8794
Abhishek Hingrajiya <abhi.h123@gmail.com>
2024-10-28 12:43:33 -0500

    bug fixes

--- processing/submit_pPb.sh
+++ processing/submit_pPb.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 #Change the path to the CMSSW_13_0_5 directory
-EXEC_PATH=${HOME}/private/analysis/CMSSW_13_0_5/src/cmsdijet/
+EXEC_PATH=${HOME}/private/analysis/CMSSW_13_0_5/src/cmsdijet
 cd ${EXEC_PATH}
 
-mkdir -p $executions_dir
+mkdir "$executions_dir"
 cp -r $EXEC_PATH/build/pPbDijetAna $executions_dir
 
