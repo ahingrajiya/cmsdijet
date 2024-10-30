535564e1ac6fdd92bd9537e86d6aab7b56685b38
Abhishek Hingrajiya <abhi.h123@gmail.com>
2024-10-30 13:40:23 -0500

    condor submission scripts are done

---
+++ processing/split_files.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+input_file_path=$1
+input_file=$2
+nfiles_per_job=$3
+
+
+cd ${input_file_path}
+
+if [ ! -f "$input_file" ]; then
+    echo "The input file does not exist"
+    exit 1
+fi
+
+NEW_DIR="${input_file%.*}"
+
+mkdir -p "${NEW_DIR}_Split"
+
+rm -f "${NEW_DIR}_Split/*"
+
+
+
+
+if [ $nfiles_per_job -eq 0 ]; then
+    nfiles_per_job=1
+fi
+
+split -l $nfiles_per_job "$input_file" "${NEW_DIR}_job/${input_file%.*}_part_" -d --additional-suffix=.txt
+
+echo "${input_file_path}/${NEW_DIR}_Split/"
+
+
--- processing/submit_pPb.sh
+++ processing/submit_pPb.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+CMS_PATH=${HOME}/private/analysis/CMSSW_13_0_5/src
+cd ${CMS_PATH}
+eval 'cmsenv'
 
 #Change the path to the CMSSW_13_0_5 directory
 EXEC_PATH=${HOME}/private/analysis/CMSSW_13_0_5/src/cmsdijet
@@ -6,6 +9,120 @@ cd ${EXEC_PATH}
 
 mkdir -p "executions_dir"
 cp -r "$EXEC_PATH/build/pPbDijetAna" "executions_dir"
+
+DataSet=$1
+isPbgoing=$2
+files_per_job=$3
+
+if [ "$DataSet" -eq 1 ]; then
+    echo "Unembedded pPb Datasset is selected"
+    cd ${EXEC_PATH}/processing
+    if [ "$isPbgoing" -eq 1 ]; then
+        echo "Pbgoing is selected"
+        condor_submit MC_unembedded_Pbgoing_submission_nopthatcut.sub
+        exit 0
+    elif [ "$isPbgoing" -eq 0 ]; then
+        echo "pgoing is selected"
+        condor_submit MC_unembedded_pgoing_submission_nopthatcut.sub
+        exit 0
+    fi
+fi
+
+if ["$DataSet" -eq 2]; then
+    echo "Embedded pPb Dataset is selected"
+    cd ${EXEC_PATH}/processing
+    if [ "$isPbgoing" -eq 1 ]; then
+        echo "Pbgoing is selected"
+        condor_submit MC_embedded_Pbgoing_submission_nopthatcut.sub
+        exit 0
+    elif [ "$isPbgoing" -eq 0 ]; then
+        echo "pgoing is selected"
+        condor_submit MC_embedded_pgoing_submission_nopthatcut.sub
+        exit 0
+    fi
+fi
+
+if ["$DataSet" -eq 3]; then
+    echo "MB pPb Dataset is selected"
+    if [ "$isPbgoing" -eq 1 ]; then
+        echo "Pbgoing is selected"
+        sample_prefilx="MB_Pbgoing"
+        input_files_list="${EXEC_PATH}/files_input/pPb_8160/DATA_MB/Pbgoing/"
+        
+    elif [ "$isPbgoing" -eq 0 ]; then
+        echo "pgoing is selected"
+        sample_prefilx="MB_pgoing"
+        input_files_list="${EXEC_PATH}/files_input/pPb_8160/DATA_MB/pgoing/"
+    fi
+fi
+
+if ["$DataSet" -eq 4]; then
+    echo "HM185 pPb Dataset is selected"
+    if [ "$isPbgoing" -eq 1 ]; then
+        echo "Pbgoing is selected"
+        sample_prefilx="HM185_Pbgoing"
+        input_files_list="${EXEC_PATH}/files_input/pPb_8160/DATA_HM185/Pbgoing/"
+        
+    elif [ "$isPbgoing" -eq 0 ]; then
+        echo "pgoing is selected"
+        sample_prefilx="HM185_pgoing"
+        input_files_list="${EXEC_PATH}/files_input/pPb_8160/DATA_HM185/pgoing/"
+    fi
+fi
+
+if ["$DataSet" -eq 5]; then
+    echo "HM185 pPb Dataset is selected"
+    if [ "$isPbgoing" -eq 1 ]; then
+        echo "Pbgoing is selected"
+        sample_prefilx="HM250_Pbgoing"
+        input_files_list="${EXEC_PATH}/files_input/pPb_8160/DATA_HM250/Pbgoing/"
+        
+    elif [ "$isPbgoing" -eq 0 ]; then
+        echo "pgoing is selected"
+        sample_prefilx="HM185_pgoing"
+        input_files_list="${EXEC_PATH}/files_input/pPb_8160/DATA_HM250/pgoing/"
+    fi
+fi
+
+if ["$DataSet" -gt 6]; then
+    echo "No Data Set Selected. Use numbers from 1 to 5"
+    exit 1
+fi
+
+PD_Number=1
+for filename in ${input_files_list}/*; do
+    echo "Processing file: $filename"
+    file_list=$(./splt_files.sh "${input_files_list}" "$filename" "$files_per_job")
+    cat <<EOF > pPb_${filename%.*}.sub
+        universe = vanilla
+        executable = ${EXEC_PATH}/run_dijetAna.sh
+        +JobFlavour           = "longlunch"
+        getenv     = True
+        requirements =((OpSysAndVer =?= "AlmaLinux9") && (CERNEnvironment =?= "qa"))
+        RequestCpus = 1
+        transfer_input_files  = voms_proxy.txt
+        environment = "X509_USER_PROXY=voms_proxy.txt"
+    EOF
+        jobid = 0
+        for file in ${file_list}; do
+        cat <<EOF >> pPb_${filename%.*}.sub
+        arguments = ${file_list}/file ${sample_prefix}_PD${PD_Number}_${jobid}.root 0 0 ${isPbgoing} 0 0
+        output = condor/logs/${sample_prefix}_PD${PD_Number}_${jobid}.out
+        error = condor/logs/${sample_prefix}_PD${PD_Number}_${jobid}.err
+        log = condor/logs/${sample_prefix}_PD${PD_Number}_${jobid}.log
+        queue
+EOF
+        jobid=$((jobid+1))
+    done
+    PD_Number=$((PD_Number+1))
+    # condor_submit pPb_${filename%.*}.sub
+done
+
+
+
+
+
+    
 
 
 
