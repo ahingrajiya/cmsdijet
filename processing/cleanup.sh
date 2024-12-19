9961dec10b41ea1b41378c676b8a5e015005abba
Abhishek Hingrajiya <abhi.h123@gmail.com>
2024-12-19 12:32:43 -0600

    added log removals

--- processing/cleanup.sh
+++ processing/cleanup.sh
@@ -3,36 +3,45 @@
 OUTPUT_PATH=/eos/user/a/ahingraj/outputs
 
 #Execution directory
-cd ${OUTPUT_PATH}
 
 Dataset=$1
 
 if [ "$Dataset" -eq 1 ]; then
     echo "Removing output UnembeddedPYTHIA files"
+    rm -f cond/*
+    cd ${OUTPUT_PATH}
     rm -f Unembedded/*.root
     exit 0
 fi
 
 if [ "$Dataset" -eq 2 ]; then
     echo "Removing output EmbeddedPYTHIA files"
+    rm -f cond/*
+    cd ${OUTPUT_PATH}
     rm -f Embedded/*.root
     exit 0
 fi
 
 if [ "$Dataset" -eq 3 ]; then
     echo "Removing output MinimumBias files"
+    rm -f condor/logs/*
+    cd ${OUTPUT_PATH}
     rm -f MB/*.root
     exit 0
 fi
 
 if [ "$Dataset" -eq 4 ]; then
     echo "Removing output HM185 files"
+    rm -f condor/logs/*
+    cd ${OUTPUT_PATH}
     rm -f HM185/*.root
     exit 0
 fi
 
 if [ "$Dataset" -eq 5 ]; then
     echo "Removing output HM250 files"
+    rm -f condor/logs/*
+    cd ${OUTPUT_PATH}
     rm -f HM250/*.root
     exit 0
 fi
