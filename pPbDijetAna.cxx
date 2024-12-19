80c5b70ea1df4cdabe5068e4f63da28dbac11053
Abhishek Hingrajiya <abhi.h123@gmail.com>
2024-12-19 15:43:39 -0600

    new bins gen reweight

--- pPbDijetAna.cxx
+++ pPbDijetAna.cxx
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     Bool_t useCMFrame{kFALSE};
     Double_t etaBoost{0.4654094531};
     TString jetBranchName{"ak4PFJetAnalyzer"};
-    std::string dijetWeightType{"Reco"};
+    std::string dijetWeightType{"Gen"};
     TString oFileName{};
     TString JECFileName{};
     TString JECFileDataName{};
