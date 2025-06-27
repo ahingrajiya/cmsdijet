9d7d48ca344df078eff01232633f77bda6f092a4
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-06-26 23:54:08 -0500

    mid-fwd dener's multiplicity check

--- pPbDijetAna.cxx
+++ pPbDijetAna.cxx
@@ -236,8 +236,8 @@ int main(int argc, char *argv[])
     analysis->setEtaBoost(etaBoost);
     analysis->setLeadJetPt(100.);
     analysis->setSubLeadJetPt(50.);
-    analysis->setLeadJetEtaRange(-3.3, -1.2);
-    analysis->setSubLeadJetEtaRange(-1., 1.);
+    analysis->setLeadJetEtaRange(-1., 1.);
+    analysis->setSubLeadJetEtaRange(1.2, 2.4);
     analysis->doInJetMultiplicity();
     analysis->setBins(multiplicityBins);
     analysis->setUEType(UEType);
