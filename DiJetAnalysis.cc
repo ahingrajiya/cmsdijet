7b494bcbbb6273c66effe438238661fcfffad9af
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-08-05 13:18:16 -0500

    Dijet power checks 1.45

--- DiJetAnalysis.cc
+++ DiJetAnalysis.cc
@@ -951,7 +951,7 @@ void DiJetAnalysis::processEvent(const Event *event)
     if (fUseDijetWeight)
     {
         fDijetWeight = DijetWeight(event);
-        fDijetWeight = pow(fDijetWeight, 1.5); // std::cout << "Dijet Weight : " << fDijetWeight << std::endl;
+        fDijetWeight = pow(fDijetWeight, 1.45); // std::cout << "Dijet Weight : " << fDijetWeight << std::endl;
     }
     else
     {
