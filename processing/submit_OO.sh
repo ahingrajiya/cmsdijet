afcdf5c88d159abb457fd9209e7f51a03c116ee8
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-08-28 14:32:30 -0500

    chnage of path for voms certificate

--- processing/submit_OO.sh
+++ processing/submit_OO.sh
@@ -63,7 +63,7 @@ for filename in ${input_files_list}/*.txt; do
         getenv          = True
         requirements    =((OpSysAndVer =?= "AlmaLinux9") && (CERNEnvironment =?= "qa"))
         RequestCpus     = 1
-        transfer_input_files  = voms_proxy.txt
+        transfer_input_files  = ${EXEC_PATH}/processing/voms_proxy.txt
         environment     = "X509_USER_PROXY=${EXEC_PATH}/processing/voms_proxy.txt"
         
 
