f59afed36e13954c4fbf4004d316dad9b437bff9
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-09-02 12:59:00 -0500

    pthat weight derivation for OO

--- ppDijetAna.cxx
+++ ppDijetAna.cxx
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
     eventCut->setMultiplicty(0, 10000);
     if (isMC)
     {
-        eventCut->setPtHat(30., 1000.);
+        eventCut->setPtHat(15., 1000.);
     }
     // eventCut->setVerbose();
 
