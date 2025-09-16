4a30d45616228ab2c1bf88522efe8db97e718d56
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-09-16 00:03:44 -0500

    added common header file

---
+++ JetEnergyScale.h
@@ -0,0 +1,28 @@
+/**
+ * @file JetEnergyScale.h
+ * @author Abhishek Hingrajiya (abhi.h123@gmail.com)
+ * @brief Derive Jet Energy Scale and Jet Energy Resolution
+ * @version 0.1
+ * @date 2025-09-15
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#ifndef JETENERGYSCALE_H
+#define JETENERGYSCALE_H
+
+#include "common.hpp"
+
+class JetEnergyScale
+{
+public:
+    JetEnergyScale();
+    virtual ~JetEnergyScale();
+
+private:
+    TFile *file_input;
+    TFile *file_output;
+};
+
+#endif // JETENERGYSCALE_H
---
+++ common.hpp
@@ -0,0 +1,26 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <cmath>
+#include "TH3.h"
+#include "TH2.h"
+#include "TH1.h"
+#include "TFile.h"
+#include "THnSparse.h"
+#include "TF1.h"
+#include "TMath.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TStyle.h"
+#include "TLatex.h"
+#include "TLine.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TGraphAsymmErrors.h"
+#include "TString.h"
+
+#endif // COMMON_HPP
