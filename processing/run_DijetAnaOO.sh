a756030c6052da3edf03f28689774d3e3345aa7f
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-09-25 10:27:31 -0500

    changes to script in accordance with runtime arguments passed

--- processing/run_DijetAnaOO.sh
+++ processing/run_DijetAnaOO.sh
@@ -13,7 +13,8 @@ cd ${EXEC_PATH}
 input_file_list=$1
 output_file=$2
 ismc=$3
+isembedded=$4
 
 
-./OODijetAna ${input_file_list} ${output_file} ${ismc}
+./OODijetAna ${input_file_list} ${output_file} ${ismc} ${isembedded}
 
