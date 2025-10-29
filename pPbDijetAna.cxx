6b31769d587de0497ee9158d826c600b8681d1a3
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-10-29 16:09:07 -0500

    changed number of events to process in pPb

--- pPbDijetAna.cxx
+++ pPbDijetAna.cxx
@@ -190,7 +190,7 @@ int main(int argc, char* argv[])
     reader->setPath2JetAnalysis(path2JEC.Data());
     reader->setUseJetID();
     reader->setJetIDType(2);
-    reader->eventsToProcess(10000);
+    reader->eventsToProcess(-1);
     reader->setJetCut(jetCut);
     reader->setTrackCut(trackCut);
     reader->setEventCut(eventCut);
