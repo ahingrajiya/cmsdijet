eb12c942056245ea49e9ebf766daaf1a53ec6f64
Abhishek Hingrajiya <abhi.h123@gmail.com>
2025-11-12 12:11:49 -0600

    changed OO job flavor for longer time

--- processing/submit_OO.sh
+++ processing/submit_OO.sh
@@ -74,7 +74,7 @@ for filename in ${input_files_list}/*.txt; do
     cat <<EOF > processing/OO_${subfile%.*}.sub
         universe        = vanilla
         executable      = ${EXEC_PATH}/processing/run_DijetAnaOO.sh
-        +JobFlavour     = "longlunch"
+        +JobFlavour     = "workday"
         getenv          = True
         requirements    =((OpSysAndVer =?= "AlmaLinux9") && (CERNEnvironment =?= "qa"))
         RequestCpus     = 1
