8f4c41f6c4c60ec6b6949c4321d7cf6f4d154297
Abhishek Hingrajiya <abhi.h123@gmail.com>
2026-02-05 22:14:37 -0600

    removed pthat cuts

--- ppDijetAna.cxx
+++ ppDijetAna.cxx
@@ -31,9 +31,9 @@ int main(int argc, char* argv[])
     TString JECFileName;
     TString JECFileDataName;
     TString path2JEC = "..";
-    Int_t collEnergyGeV{5020};
+    Int_t collEnergyGeV{5360};
     TString collSystem{"pp"};
-    Int_t collYear{2018};
+    Int_t collYear{2025};
     Bool_t useCentWeight{kFALSE};
     TString jetBranchName{"ak4PFJetAnalyzer"};
     std::vector<std::pair<Double_t, Double_t>> multiplicityBins = {{0, 0.0}, {10, 1.0}, {60, 2.0}, {120, 3.0}, {185, 4.0}, {250, 5.0}, {400, 6.0}};
@@ -85,16 +85,16 @@ int main(int argc, char* argv[])
     EventCut* eventCut = new EventCut{};
     eventCut->setVz(-15., 15.);
     eventCut->setMultiplicty(0, 10000);
-    if (isMC)
-    {
-        eventCut->setPtHat(50., 1000.);
-    }
+    // if (isMC)
+    // {
+    //     eventCut->setPtHat(10., 10000.);
+    // }
     // eventCut->setVerbose();
 
     // Initialize jet cuts
     JetCut* jetCut = new JetCut{};
     jetCut->setEta(-5.0, 5.0);
-    jetCut->setPt(0., 5020.);
+    jetCut->setPt(0., 5360.);
     // jetCut->setVerbose();
 
     // Initialize track cuts
@@ -154,13 +154,13 @@ int main(int argc, char* argv[])
     analysis->addHistoManager(hm);
     analysis->setReader(reader);
     analysis->setIsMC(isMC);
-    analysis->setLeadJetEtaRange(-1.6, 1.6);
-    analysis->setSubLeadJetEtaRange(-1.6, 1.6);
+    analysis->setLeadJetEtaRange(-2.1, 2.1);
+    analysis->setSubLeadJetEtaRange(-2.1, 2.1);
     analysis->setMultiplicityRange(0., 10000.);
     analysis->setMultiplicityType(0);
-    analysis->setLeadJetPt(100.);
-    analysis->setSubLeadJetPt(50.);
-    analysis->setDeltaPhi(5. * TMath::Pi() / 6);
+    analysis->setLeadJetPt(50.);
+    analysis->setSubLeadJetPt(20.);
+    analysis->setDeltaPhi(7. * TMath::Pi() / 8);
     analysis->setInclusiveCorrectedJetPtMin(50.);
     analysis->setInclusiveJetEtaRange(-2.1, 2.1);
     analysis->setMinTrkPt(0.5);
