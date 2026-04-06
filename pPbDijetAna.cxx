5457a9f6af777c8072351593a7aa073b9d442e91
Abhishek Hingrajiya <abhi.h123@gmail.com>
2026-04-06 16:56:41 -0500

    changed the dijet kinetic selection for resolution check

--- pPbDijetAna.cxx
+++ pPbDijetAna.cxx
@@ -241,8 +241,8 @@ int main(int argc, char* argv[])
     analysis->setDeltaPhi(5 * TMath::Pi() / 6);
     analysis->setUseCMFrame();
     analysis->setEtaBoost(etaBoost);
-    analysis->setLeadJetPt(50.);
-    analysis->setSubLeadJetPt(30.);
+    analysis->setLeadJetPt(60.);
+    analysis->setSubLeadJetPt(40.);
     analysis->setLeadJetEtaRange(-2.1, 2.1);
     analysis->setSubLeadJetEtaRange(-2.1, 2.1);
     analysis->setInclusiveJetEtaRange(-2.1, 2.1);
