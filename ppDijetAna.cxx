3ef28f85cf0caf06f106d91ca0469dc3ecc3b250
Abhishek Hingrajiya <abhi.h123@gmail.com>
2026-04-06 09:52:02 -0500

    chnaged kinetic selections

--- ppDijetAna.cxx
+++ ppDijetAna.cxx
@@ -174,9 +174,9 @@ int main(int argc, char* argv[])
     analysis->setSubLeadJetEtaRange(-2.1, 2.1);
     analysis->setMultiplicityRange(0., 10000.);
     analysis->setMultiplicityType(0);
-    analysis->setLeadJetPt(100.);
-    analysis->setSubLeadJetPt(50.);
-    analysis->setDeltaPhi(5. * TMath::Pi() / 6);
+    analysis->setLeadJetPt(50.);
+    analysis->setSubLeadJetPt(30.);
+    analysis->setDeltaPhi(2.7);
     analysis->setInclusiveCorrectedJetPtMin(20.);
     analysis->setInclusiveJetEtaRange(-2.1, 2.1);
     analysis->setMinTrkPt(0.5);
