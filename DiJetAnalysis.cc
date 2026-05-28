8547017700e39b2f970f3034a5d630b9e772d28b
Abhishek Hingrajiya <abhi.h123@gmail.com>
2026-05-28 16:12:53 -0500

    checking with -5% weight tuning

--- DiJetAnalysis.cc
+++ DiJetAnalysis.cc
@@ -778,7 +778,7 @@ Float_t DiJetAnalysis::DijetWeight(const Event* event)
                 float xj = Asymmetry(genLeadJetPt, genSubLeadJetPt);
                 if (xj > 0.9 && dijetWeight != 1.0)
                 {
-                    dijetWeight += -0.03 * dijetWeight;
+                    dijetWeight += -0.05 * dijetWeight;
                 }
             }
             else
