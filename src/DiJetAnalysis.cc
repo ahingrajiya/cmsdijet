3a7f82e3194e98f03f8043c6f268df6faca3a302
Abhishek Hingrajiya <abhi.h123@gmail.com>
2026-07-07 14:05:30 -0500

    weight check

--- src/DiJetAnalysis.cc
+++ src/DiJetAnalysis.cc
@@ -1787,7 +1787,7 @@ float DiJetAnalysis::jetPtWeight(const double& recoLeadPt)
 }
 double DiJetAnalysis::getWeightMultiplier(double xj)
 {
-    return 1.0 - (0.20 / (1.0 + std::exp(-20. * (xj - 1.0))));
+    return 1.0 - (0.10 / (1.0 + std::exp(-20. * (xj - 1.0))));
 }
 
 void DiJetAnalysis::report()
